425b65895afb7f3facdc3ce78efb12d9f966af66
Survivor <dongchen1022@gmail.com>
2016-12-08 18:32:12 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,88 @@
+<template>
+  <div id="app">
+    <el-row>
+      <haibao></haibao>
+    </el-row>
+    <el-row>
+      <el-col :span="4">
+        <el-button @click="addProd()">添加新的商品</el-button>
+      </el-col>
+      <el-col :span="4">
+        <el-button type="text">查看已上线商品</el-button>
+      </el-col>
+      <el-col :span="4">
+        <el-button type="text" @click="addZhuliNum()">添加助力人数</el-button>
+      </el-col>
+    </el-row>
+    <el-row>
+      <el-col>
+        <p>是否开启请求科玛进行新用户验证<p>
+        <p>当前状态: {{currentState}} <el-button @click="toggleState()">{{btnState}}</el-button></p>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+// import Hello from './components/Hello'
+import Haibao from './components/haibao'
+
+export default {
+  name: 'app',
+  components: {
+    Haibao
+  },
+  computed: {
+    currentState () {
+      return this.state ? '已开启' : '已关闭'
+    },
+    btnState () {
+      return this.state ? '关闭' : '开启'
+    }
+  },
+  data () {
+    return {
+      state: true,
+      addUrl: 'http://pin.haibaozhuli.test.willar.net/manage/addZhuliNumber',
+      url: 'http://market.cmbc.test.youngmanager.cn/test4'
+    }
+  },
+  methods: {
+    addProd () {
+      console.log('add')
+    },
+
+    toggleState () {
+      this.state = !this.state
+    },
+    addZhuliNum () {
+      this.$http.post(this.url).then((response) => {
+        console.log('success callback')
+      }, (response) => {
+        console.log('error callback')
+      })
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.el-row {
+  margin-bottom: 10px;
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+
+.el-col {
+  text-align: left;
+}
+</style>
---
+++ src/components/haibao.vue
@@ -0,0 +1,128 @@
+<template>
+  <el-table
+    :data="tableData"
+    height="700"
+    border
+    style="width: 100%">
+    <el-table-column
+      prop="name"
+      label="商品名称"
+      width="100">
+    </el-table-column>
+    <el-table-column
+      prop="pid"
+      label="商品编码">
+    </el-table-column>
+    <el-table-column
+      prop="state"
+      label="状态">
+    </el-table-column>
+    <el-table-column
+      prop="endline"
+      label="下线时间">
+    </el-table-column>
+    <el-table-column
+      prop="id"
+      label="序号">
+    </el-table-column>
+    <el-table-column
+      prop="num"
+      label="所需助力人数">
+    </el-table-column>
+    <el-table-column
+      prop="time"
+      label="用户单纯参加活动时间完成限制">
+    </el-table-column>
+    <el-table-column
+      inline-template
+      prop="pic1"
+      label="列表页内展示图"
+      width="200">
+      <div>
+        <img :src="row.pic1" alt="" width="180">
+      </div>
+    </el-table-column>
+    <el-table-column
+      inline-template
+      prop="pic2"
+      label="海报背景图"
+      width="200">
+      <div>
+        <img :src="row.pic2" alt="" width="180">
+      </div>
+    </el-table-column>
+    <el-table-column
+      inline-template
+      prop="btn1"
+      label="正常按钮">
+      <div>
+        <img :src="row.btn1" alt="" width="50">
+      </div>
+    </el-table-column>
+    <el-table-column
+      inline-template
+      prop="btn2"
+      label="即将到期按钮">
+      <div>
+        <img :src="row.btn2" alt="" width="50">
+      </div>
+    </el-table-column>
+    <el-table-column
+      inline-template
+      prop="pic3"
+      label="详情页内展示图"
+      width="200">
+      <div>
+        <img :src="row.pic3" alt="" width="180">
+      </div>
+    </el-table-column>
+    <el-table-column
+      inline-template
+      label="操作">
+      <div>
+        <el-button type="text">修改</el-button>
+      </div>
+    </el-table-column>
+  </el-table>
+</template>
+
+<script>
+export default {
+  name: 'haibao',
+  data () {
+    return {
+      tableData: [{
+        name: '清风原木纯品2层150抽4包抽面纸',
+        pid: 'ONE',
+        state: '正常上线',
+        endline: '2016-12-18 00:00:00',
+        id: '1',
+        num: '30',
+        time: '72小时',
+        pic1: 'http://pin.haibaozhuli.cdn.willar.net/pin1212_test/entranceImg/20161207101159_ONE_entrance.png',
+        pic2: 'http://pin.haibaozhuli.cdn.willar.net/pin1212_test/background/20161207100214_ONE_posterBg.png',
+        btn1: 'http://pin.haibaozhuli.cdn.willar.net/pin1212_test/buttonStartedImg/20161207100214_ONE_btnStarted.png',
+        btn2: 'http://pin.haibaozhuli.cdn.willar.net/pin1212_test/buttonEndImg/20161207100214_ONE_btnEnd.png',
+        pic3: 'http://pin.haibaozhuli.cdn.willar.net/pin1212_test/detailImg/20161207100214_ONE_detail.png'
+      }, {
+        name: '清风原木纯品2层150抽4包抽面纸',
+        pid: 'ONE',
+        state: '正常上线',
+        endline: '2016-12-18 00:00:00',
+        id: '1',
+        num: '30',
+        time: '72小时',
+        pic1: 'http://pin.haibaozhuli.cdn.willar.net/pin1212_test/entranceImg/20161207101159_ONE_entrance.png',
+        pic2: 'http://pin.haibaozhuli.cdn.willar.net/pin1212_test/background/20161207100214_ONE_posterBg.png',
+        btn1: 'http://pin.haibaozhuli.cdn.willar.net/pin1212_test/buttonStartedImg/20161207100214_ONE_btnStarted.png',
+        btn2: 'http://pin.haibaozhuli.cdn.willar.net/pin1212_test/buttonEndImg/20161207100214_ONE_btnEnd.png',
+        pic3: 'http://pin.haibaozhuli.cdn.willar.net/pin1212_test/detailImg/20161207100214_ONE_detail.png'
+      }]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
