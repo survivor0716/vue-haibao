0c4badd5c6e3e43b2b506b00248bfc03481aa9de
Survivor <dongchen1022@gmail.com>
2016-12-12 18:22:01 +0800

    Add vue-router, element componet: form, tabs

--- src/components/haibao.vue
+++ src/components/haibao.vue
@@ -87,37 +87,12 @@
 </template>
 
 <script>
+import Haibao from './HaibaoData'
 export default {
   name: 'haibao',
   data () {
     return {
-      tableData: [{
-        name: '清风原木纯品2层150抽4包抽面纸',
-        pid: 'ONE',
-        state: '正常上线',
-        endline: '2016-12-18 00:00:00',
-        id: '1',
-        num: '30',
-        time: '72小时',
-        pic1: 'http://pin.haibaozhuli.cdn.willar.net/pin1212_test/entranceImg/20161207101159_ONE_entrance.png',
-        pic2: 'http://pin.haibaozhuli.cdn.willar.net/pin1212_test/background/20161207100214_ONE_posterBg.png',
-        btn1: 'http://pin.haibaozhuli.cdn.willar.net/pin1212_test/buttonStartedImg/20161207100214_ONE_btnStarted.png',
-        btn2: 'http://pin.haibaozhuli.cdn.willar.net/pin1212_test/buttonEndImg/20161207100214_ONE_btnEnd.png',
-        pic3: 'http://pin.haibaozhuli.cdn.willar.net/pin1212_test/detailImg/20161207100214_ONE_detail.png'
-      }, {
-        name: '清风原木纯品2层150抽4包抽面纸',
-        pid: 'TWO',
-        state: '正常上线',
-        endline: '2016-12-18 00:00:00',
-        id: '2',
-        num: '30',
-        time: '72小时',
-        pic1: 'http://pin.haibaozhuli.cdn.willar.net/pin1212_test/entranceImg/20161207101159_ONE_entrance.png',
-        pic2: 'http://pin.haibaozhuli.cdn.willar.net/pin1212_test/background/20161207100214_ONE_posterBg.png',
-        btn1: 'http://pin.haibaozhuli.cdn.willar.net/pin1212_test/buttonStartedImg/20161207100214_ONE_btnStarted.png',
-        btn2: 'http://pin.haibaozhuli.cdn.willar.net/pin1212_test/buttonEndImg/20161207100214_ONE_btnEnd.png',
-        pic3: 'http://pin.haibaozhuli.cdn.willar.net/pin1212_test/detailImg/20161207100214_ONE_detail.png'
-      }],
+      tableData: Haibao,
       tableData1: {}
     }
   },
--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Routes from './router.js'
+
 import VueResource from 'vue-resource'
-import App from './App'
+
 import 'element-ui/lib/theme-default/index.css'
 import ElementUI from 'element-ui'
 
+import App from './App'
+
+Vue.use(VueRouter)
 Vue.use(VueResource)
 Vue.use(ElementUI)
 
@@ -13,8 +19,14 @@ Vue.http.options.emulateJSON = true
 
 // Vue.http.options.emulateHTTP = true
 
+const router = new VueRouter({
+  mode: 'history',
+  routes: Routes
+})
+
 /* eslint-disable no-new */
 new Vue({
+  router,
   el: '#app',
   template: '<App/>',
   components: { App }
